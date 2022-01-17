293ca93ee3d926f3013b601e2002ebc8136d49bc
Saboor <saboor547@gmail.com>
2022-01-16 18:00:51 -0700

    recolor cuz dumb

--- functions/getUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = client => {
-
-	client.getUser = async function get(args) {
-		const users = await client.query(`SELECT * FROM Users WHERE userID = ${args}`);
-		return users[0];
-	};
-
-
-};
