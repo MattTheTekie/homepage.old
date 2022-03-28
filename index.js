c5bd39b2b17846560c0c2a1e637b9ac4860edffd
Saboor <saboor547@gmail.com>
2022-03-27 22:31:36 -0600

    switch to github pages

--- index.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const client = {};
-client.startTimestamp = Date.now();
-client.config = require('./config');
-for (const handler of fs.readdirSync('./handlers').filter(file => file.endsWith('.js'))) require(`./handlers/${handler}`)(client);
