a3ad676b7f4b8f0e22d6f70e6bdde8ffb9e89c41
saboooor <saboor547@gmail.com>
2022-12-22 12:29:31 -0700

    a

--- express.js
+++ express.js
@@ -4,4 +4,9 @@ const path = require('path');
 const app = express();
 app.use('/files', express.static(path.resolve('files')));
 app.get('/files', (req, res) => res.redirect('/filehost'));
+app.get('/files/iplogger', (req, res) => {
+    // Log the user ip address
+    console.log(req.ip);
+    res.redirect('/');
+});
 app.listen(config.filesport, () => console.log(`Files hosted at: http://localhost:${config.filesport}/`));
--- src/routes/iplogger/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from '@builder.io/qwik-city';
-
-export const onGet: RequestHandler<any> = async ({ request }) => {
-    // log ip
-    const ip = request.headers.get('x-forwarded-for');
-    console.log(ip);
-};
