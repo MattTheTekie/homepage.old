7d283d3f7cb9f2f7f303f22a72927715badfd269
saboooor <saboor547@gmail.com>
2023-03-22 21:34:15 -0600

    im boutta cloudflare u up :lipbite:

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import config from './config.json';
-
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
@@ -20,20 +18,6 @@ export default defineConfig(() => {
       headers: {
         'Cache-Control': 'public, max-age=600',
       },
-      hmr: {
-          clientPort: config.port
-      },
-      port: config.port,
-      strictPort: true,
-      host: "0.0.0.0"
     },
-    server: {
-      hmr: {
-          clientPort: config.port
-      },
-      port: config.port,
-      strictPort: true,
-      host: "0.0.0.0"
-    }
   };
 });
