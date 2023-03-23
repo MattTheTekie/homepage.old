4ad4134accffabebb0e5afa1a1c36774d5527415
saboooor <saboor547@gmail.com>
2023-03-22 21:30:43 -0600

    ur job is not to serve transcripts anymore

--- express.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const config = require('./config');
-const path = require('path');
-const app = express();
-app.use('/files', express.static(path.resolve('files')));
-app.get('/files', (req, res) => res.redirect('/filehost'));
-app.listen(config.filesport, () => console.log(`Files hosted at: http://localhost:${config.filesport}/`));
