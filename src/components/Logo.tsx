52b7b62f2b286ad5bc9e33bad961b3e186a789ba
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-05-13 16:08:41 -0400

    Add files via upload

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 
-/**
- * The RouterHead component is placed inside of the document `<head>` element.
- */
 export default component$(() => {
   return (
     <svg
