15fb780367ae41320873ed7943c50064a4fb65b0
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-05-13 14:45:24 -0400

    Update Head.tsx

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -10,13 +10,13 @@ export const RouterHead = component$(() => {
 
   return (
     <>
-      <title>{`Luminescent - ${head.title}`}</title>
-      <meta content={`Luminescent - ${head.title}`} property="og:title"/>
+      <title>{`MattTheTekie - ${head.title}`}</title>
+      <meta content={`MattTheTekie - ${head.title}`} property="og:title"/>
       <meta content="#0D0D0D" name="theme-color"/>
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/png" href="https://avatars.githubusercontent.com/u/86643576" />
+      <link rel="icon" type="image/png" href="https://avatars.githubusercontent.com/u/94870997" />
 
       {head.meta.map((m) => <>
         <meta {...m} />
