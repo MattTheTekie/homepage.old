e7745f7a95cfc6beb5ed0c85037306b1ee64dc33
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-05-14 08:37:32 -0400

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -43,7 +43,7 @@ export default component$(() => {
           <NavButton external mobile icon href="https://github.com/MattTheTekie" title="GitHub" extraClass="flex xl:hidden">
             <LogoGithub width="24" class="fill-purple-200" />
           </NavButton>
-          <NavButton mobile icon href="https://discord.com/invite/VhfM3UncBB" title="Discord" extraClass="flex xl:hidden">
+          <NavButton external mobile icon href="https://discord.com/invite/VhfM3UncBB" title="Discord" extraClass="flex xl:hidden">
             <LogoDiscord width="24" class="fill-indigo-200" />
           </NavButton>
         </div>
