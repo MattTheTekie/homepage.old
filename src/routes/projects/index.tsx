caa9529401f502a83e49d1d3d9cec7d937fa2979
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-05-14 08:45:03 -0400

    Update index.tsx

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -16,7 +16,7 @@ export default component$(() => {
         <Card name="Dragon TV" icon="https://raw.githubusercontent.com/MattTheTekie/MattTheTekie/main/Personal-Scripts/Images/Resized/dragon_460x460.jpeg" github="https://github.com/MattTheTekie/MattTheTekie/tree/main/Personal-Scripts/DRAGON_TV" link="https://github.com/MattTheTekie/MattTheTekie/tree/main/Personal-Scripts/DRAGON_TV">
           Dragon TV is my web TV station around town. It's pretty epic.
         </Card>
-        <Card name="Dragon Linux" icon="https://dragon-linux.surge.sh/img/dragon.jpeg" github="https://github.com/MattTheTekie/MattTheTekie/tree/main/Personal-Scripts/Distromaking" link="https://freedom-hq.github.io/Dragon-Linux/">
+        <Card name="Dragon Linux" icon="https://raw.githubusercontent.com/MattTheTekie/MattTheTekie/main/Personal-Scripts/Images/Resized/static.displate.com_800be04aa6f899076275e4dd96dbf9f4_46e904de13eaa602b904b8a5fa5e3e23_460x460.jpg" github="https://github.com/MattTheTekie/MattTheTekie/tree/main/Personal-Scripts/Distromaking" link="https://freedom-hq.github.io/Dragon-Linux/">
           Dragon Linux, is a awesome Linux distro I made many years back.
         </Card>
         <Card name="HomeLab" icon="https://cdn.discordapp.com/avatars/796935151417688074/6202f5e5cc62481162ef0a98b364f702.webp?size=2048" github="https://github.com/MattTheTekie/HomeLab" link="https://tsukashomelab.netlify.app/">
