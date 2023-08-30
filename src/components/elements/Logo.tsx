e366415c1b43a8b628a5a36e0665884458a62929
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-08-30 17:07:20 -0400

    Update Logo.tsx

--- src/components/elements/Logo.tsx
+++ src/components/elements/Logo.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <div>
-      <img src="https://files.catbox.moe/e2f8xf.webp" />
+      <img src="https://files.catbox.moe/23hk4t.webp" />
     </div>
   );
 });
